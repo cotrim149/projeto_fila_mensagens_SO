4b2d52d309662d4107b527509596b36619412b9e
cotrim <cotrim149@gmail.com>
2015-04-27 23:58:42 -0300

    Nova versao leitor de cartao, agora com MPI

--- codigo/mpi/leitor_cartao.c
+++ codigo/mpi/leitor_cartao.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <regex.h>
 
+#include "mpi.h"
+
 #define NUM_CARDS 10
 #define SIZE_MSG 125
 
@@ -13,8 +15,9 @@ typedef struct{
 void readCards();
 void replaceAsterisk();
 void printCards();
+MPI_Datatype createNewType();
 
-int main(){
+int main(int argc,char* argv[]){
 	Card cards[NUM_CARDS];
 	int i;
 	
@@ -22,13 +25,49 @@ int main(){
 		cards[i].msg = (char*)malloc(sizeof(char)*SIZE_MSG);
 	}
 
-	readCards(cards);
-	replaceAsterisk();
-	printCards();
+	int numtasks,rank;
+	int num_elements_msg = 1;
+	MPI_Status stats;
+
+	MPI_Init(&argc,&argv);
+	MPI_Comm_size(MPI_COMM_WORLD,&numtasks);
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 	
+	MPI_Datatype cardDatatype = createNewType();
+	
+	switch(rank){
+	case 0:
+		printf("\nPROCESSO RANK 0 EM ACAO\n");
+		readCards(cards,cardDatatype);
+	break;
+	case 1:
+		printf("\nPROCESSO RANK 1 EM ACAO\n");
+		replaceAsterisk(cardDatatype);
+	break;
+	case 2:
+		printf("\nPROCESSO rank 2 EM ACAO\n");
+		printCards(cardDatatype);
+	break;
+	}	
+	
+	MPI_Finalize();
+
 return 0;
 }
 
+MPI_Datatype createNewType(){
+	int count = 1;
+	int lengths[1] = {SIZE_MSG};
+	
+	MPI_Aint offsets[1] = {0};
+	MPI_Datatype types[1] = {MPI_CHAR};
+
+	MPI_Datatype cardDatatype;
+	MPI_Type_create_struct(count, lengths, offsets, types, &cardDatatype);
+	MPI_Type_commit(&cardDatatype);
+	
+	return cardDatatype;
+}
 
 FILE* open_file(char* path_file, char* mode){
 
@@ -43,11 +82,7 @@ FILE* open_file(char* path_file, char* mode){
 	return file;
 }
 
-void readCards(Card* cards){
-	/*	
-		gerar String de caracters com espaço em branco entre imagens
-		colocar string em arquivo temporario 1
-	*/
+void readCards(Card* cards,MPI_Datatype cardDatatype){
 	// open file cards.txt
 	char path[] = "cards.txt";
 	FILE *cards_file = open_file(path,"r");
@@ -57,23 +92,35 @@ void readCards(Card* cards){
 	for(countLine=0;countLine<NUM_CARDS;countLine++){
 		fscanf(cards_file,"%s \n",cards[countLine].msg);	
 	}
-
-	for(countLine=0;countLine<NUM_CARDS;countLine++){
+	
+	//leitura cartao
+	for(countLine=0;countLine<2;countLine++){
+//	for(countLine=0;countLine<NUM_CARDS;countLine++){
 		printf("Cartao %d: %s\n",countLine,cards[countLine].msg);
 	}
 	
 	fclose(cards_file);
+	
+	int dest = 1;
+	int tag = 0;
+	int num_elements_msg = 1;
 
-	char path_temp_1[] = "temp_1.txt";	
-	FILE *temp_1 = open_file(path_temp_1,"a+");
+	MPI_Send(cards[0].msg,num_elements_msg,cardDatatype,dest,tag,MPI_COMM_WORLD);
+	MPI_Send(cards[1].msg,num_elements_msg,cardDatatype,dest,tag,MPI_COMM_WORLD);
+	printf("Processo 0(Leitura de msg) enviou %s para %d(replaceAsterisk).\n", cards[0].msg, dest);
 
-	for(countLine=0;countLine<NUM_CARDS;countLine++){
-		char *temp_card = cards[countLine].msg;
-		printf("Escrevendo em temp_1: %s \n",temp_card);
-		fprintf(temp_1,"%s ",temp_card);
-	}
+	
+	// arquivo temporario 1
+/*	char path_temp_1[] = "temp_1.txt";	*/
+/*	FILE *temp_1 = open_file(path_temp_1,"a+");*/
 
-	fclose(temp_1);
+/*	for(countLine=0;countLine<NUM_CARDS;countLine++){*/
+/*		char *temp_card = cards[countLine].msg;*/
+/*		printf("Escrevendo em temp_1: %s \n",temp_card);*/
+/*		fprintf(temp_1,"%s ",temp_card);*/
+/*	}*/
+
+/*	fclose(temp_1);*/
 	int i;
 	for(i=0;i<NUM_CARDS;i++){
 		free(cards[i].msg);
@@ -164,61 +211,91 @@ void regex_match_replace(char *regex, char* info, int card_number)
 
 }
 
-void replaceAsterisk(){
-	/*	
-		colocar resultado em um arquivo temporario 2
-	*/
+void replaceAsterisk(MPI_Datatype cardDatatype){
 	
-	char *path_temp_1 = "temp_1.txt";
-	FILE* temp_1 = open_file(path_temp_1,"r");
+/*	char *path_temp_1 = "temp_1.txt";*/
+/*	FILE* temp_1 = open_file(path_temp_1,"r");*/
 	
 	Card cards[NUM_CARDS];	
 	int i,countLine;
 	
+	//alocando memoria para variavel	
 	for(i=0;i<NUM_CARDS;i++){
 		cards[i].msg = (char*)malloc(sizeof(char)*SIZE_MSG);
 	}
+	// passando info de arquivo para variavel
+/*	for(countLine=0;countLine<NUM_CARDS;countLine++){*/
+/*		fscanf(temp_1,"%s ",cards[countLine].msg);	*/
+/*	}*/
 	
-	for(countLine=0;countLine<NUM_CARDS;countLine++){
-		fscanf(temp_1,"%s ",cards[countLine].msg);	
-	}
-	
-	fclose(temp_1);
-	
-	for(countLine=0;countLine<NUM_CARDS;countLine++){
+/*	fclose(temp_1);*/
+
+	// Recebendo msg
+	int source =0; 
+	int tag = 0;
+	int num_elements_msg = 1;
+	MPI_Status stats;
+	MPI_Recv(cards[0].msg,num_elements_msg,cardDatatype, source, tag, MPI_COMM_WORLD,&stats);
+	MPI_Recv(cards[1].msg,num_elements_msg,cardDatatype, source, tag, MPI_COMM_WORLD,&stats);
+	printf("Processo 1(replaceAsterisk) recebeu %s de %d.\n", cards[0].msg, source);
+
+	for(countLine=0;countLine<2;countLine++){
+//	for(countLine=0;countLine<NUM_CARDS;countLine++){
 		regex_match_replace("\\*\\*", cards[countLine].msg,countLine);	
-//		printf("Cartao modificado %d: %s \n",countLine,cards[countLine].msg);	
+		printf("Cartao modificado %d: %s \n",countLine,cards[countLine].msg);	
 	}
 
-	char* path_temp_2 = "temp_2.txt";
-	FILE* temp_2 = open_file(path_temp_2,"w+");
+	int dest = 2;
+
+	MPI_Send(cards[0].msg,num_elements_msg,cardDatatype,dest,tag,MPI_COMM_WORLD);
+	MPI_Send(cards[1].msg,num_elements_msg,cardDatatype,dest,tag,MPI_COMM_WORLD);
+	printf("Processo 1(replaceAsterisk) enviou %s para %d(printCards).\n", cards[0].msg, dest);
+
 	
-	for(countLine=0;countLine<NUM_CARDS;countLine++){
-		char *temp_card = cards[countLine].msg;
-		fprintf(temp_2,"%s ",temp_card);
-	}
-	
-	fclose(temp_2);
+
+
+	// abertura de arquivo temporario 2
+/*	char* path_temp_2 = "temp_2.txt";*/
+/*	FILE* temp_2 = open_file(path_temp_2,"w+");*/
+/*	*/
+/*	for(countLine=0;countLine<NUM_CARDS;countLine++){*/
+/*		char *temp_card = cards[countLine].msg;*/
+/*		fprintf(temp_2,"%s ",temp_card);*/
+/*	}*/
+/*	*/
+/*	fclose(temp_2);*/
 	
 }
 
-void printCards(){
-
-	char* path_temp_2 = "temp_2.txt";
-	FILE* temp_2 = open_file(path_temp_2,"r");
+void printCards(MPI_Datatype cardDatatype){
+		
+	// abertura arquivo temporario 2
+/*	char* path_temp_2 = "temp_2.txt";*/
+/*	FILE* temp_2 = open_file(path_temp_2,"r");*/
 	Card cards[NUM_CARDS];
 	int i, countLine;
 	for(i=0;i<NUM_CARDS;i++){
 		cards[i].msg = (char*)malloc(sizeof(char)*SIZE_MSG);
 	}
 
-	for(countLine=0;countLine<NUM_CARDS;countLine++){
-		fscanf(temp_2,"%s ",cards[countLine].msg);	
-	}
+	// Recebendo msg
+	int source =1; 
+	int tag = 0;
+	int num_elements_msg = 1;
+	MPI_Status stats;
+	MPI_Recv(cards[0].msg,num_elements_msg,cardDatatype, source, tag, MPI_COMM_WORLD,&stats);
+	MPI_Recv(cards[1].msg,num_elements_msg,cardDatatype, source, tag, MPI_COMM_WORLD,&stats);
+	printf("Processo 2(printCards) recebeu %s de %d(replaceAsterisk).\n", cards[0].msg, source);
 
-	for(i=0;i<NUM_CARDS;i++){
+
+/*	for(countLine=0;countLine<NUM_CARDS;countLine++){*/
+/*		fscanf(temp_2,"%s ",cards[countLine].msg);	*/
+/*	}*/
+
+//	for(i=0;i<NUM_CARDS;i++){
+	for(i=0;i<2;i++){
 		printf("Cartao %d: %s\n",i,cards[i].msg);
 	}
-	system("rm temp_1.txt temp_2.txt");
+//	system("rm temp_1.txt temp_2.txt");
 }
 
